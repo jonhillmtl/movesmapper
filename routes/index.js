2064ff0daedb84db4589150d54deca54523db8d5
Jon Hill <jon@jonhill.ca>
2014-06-25 22:45:21 -0400

    more

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ router.get('/', function(req, res) {
     var client_id = '3dM7jup81_5gWa5WrEgVeM162OOHMITX';
     var authenticate_url = 
         "https://api.moves-app.com/oauth/v1/authorize?response_type=code&client_id={0}&scope=activity,location".format(client_id);
-    res.render('index', { title: 'Moves Mapper', client_id : authenticate_url });
+    res.render('index', { title: 'Moves Mapper', authenticate_url : authenticate_url });
 });
 
 module.exports = router;

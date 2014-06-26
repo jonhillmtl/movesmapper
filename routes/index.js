3fcf6e9c97f8340cfc4a59b2a3cd1ab5d6ce3f42
Jon Hill <jon@jonhill.ca>
2014-06-25 22:42:38 -0400

    yep

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Moves Mapper', client_id : '3dM7jup81_5gWa5WrEgVeM162OOHMITX' });
+    var client_id = '3dM7jup81_5gWa5WrEgVeM162OOHMITX';
+    var authenticate_url = 
+        "https://api.moves-app.com/oauth/v1/authorize?response_type=code&client_id={0}&scope=activity,location".format(client_id);
+    res.render('index', { title: 'Moves Mapper', client_id : authenticate_url });
 });
 
 module.exports = router;

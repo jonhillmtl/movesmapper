19d48ef76809c7f0121e4f31485a4c8765ac8b48
Jon Hill <jon@jonhill.ca>
2014-06-25 22:39:41 -0400

    more

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Moves Mapper' });
+  res.render('index', { title: 'Moves Mapper', client_id : '3dM7jup81_5gWa5WrEgVeM162OOHMITX' });
 });
 
 module.exports = router;

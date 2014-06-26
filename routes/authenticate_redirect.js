3ae21f74d585f8373615f83adc695fa922cb4d1f
Jon Hill <jon@jonhill.ca>
2014-06-25 23:24:12 -0400

    more

--- routes/authenticate_redirect.js
+++ routes/authenticate_redirect.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var util = require('util');
+var request = require('request');
 
 /* GET authenticate redirect */
 router.get('/', function(req, res) {
-    res.render('authenticate_redirect', { title: 'Authenticate Redirect', code : req.query.code});
+    var access_token_url = util.format(
+        'https://api.moves-app.com/oauth/v1/access_token?grant_type=authorization_code&code=%s&client_id=%s&client_secret=%s',
+        req.query.code,
+        process.env['CLIENT_ID'],
+        process.env['CLIENT_SECRET']);
+
+    var access_token_body = '';
+    request.post(
+        access_token_url,
+        { form: { key: 'value' } },
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) 
+            {
+                access_token_body = body
+                console.log(body)
+            }
+        }
+    );
+    res.render('authenticate_redirect', { title: 'Authenticate Redirect', access_token_body : access_token_body});
 });
 
 module.exports = router;

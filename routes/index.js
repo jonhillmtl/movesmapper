4c749711be0b138e4a79f9d138ae91106e523db4
Jon Hill <jon@jonhill.ca>
2014-06-25 23:16:58 -0400

    more

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
-String.prototype.format = function() {
-    var formatted = this;
-    for (var i = 0; i < arguments.length; i++) {
-        var regexp = new RegExp('\\{'+i+'\\}', 'gi');
-        formatted = formatted.replace(regexp, arguments[i]);
-    }
-    return formatted;
-};
+require('util');
 
 /* GET home page. */
 router.get('/', function(req, res) {
     var client_id = process.env['CLIENT_ID'];
-    var authenticate_url = 
-        "https://api.moves-app.com/oauth/v1/authorize?response_type=code&client_id={0}&scope=activity%20location".format(client_id);
+    var authenticate_url = util.format(
+        "https://api.moves-app.com/oauth/v1/authorize?response_type=code&client_id=%s&scope=activity%20location", 
+        client_id);
     res.render('index', { title: 'Moves Mapper', authenticate_url : authenticate_url });
 });
 

948629dfdd5e083d5bbf69fad5e569e7d596fabf
Jon Hill <jon@jonhill.ca>
2014-06-26 00:07:32 -0400

    more

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.cookieParser());
-app.use(express.session({secret : process.env['SESSION_SECRET']});
+app.use(express.session({secret : process.env['SESSION_SECRET']}));
 
 app.use('/', routes);
 app.use('/users', users);

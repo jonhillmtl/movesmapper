de8ad3ca9fd77e9781739a8875a94445ec3272ce
Jon Hill <jon@jonhill.ca>
2014-06-25 23:07:04 -0400

    more

--- routes/authenticate_redirect.js
+++ routes/authenticate_redirect.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET authenticate redirect */
 router.get('/', function(req, res) {
-    res.render('authenticate_redirect', { title: 'Authenticate Redirect'});
+    res.render('authenticate_redirect', { title: 'Authenticate Redirect', code : req.query.code});
 });
 
 module.exports = router;

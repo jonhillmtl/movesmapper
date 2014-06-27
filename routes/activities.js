19c62df20f40cad0311b5bf6ead2b2ed93cf715a
Jon Hill <jon@jonhill.ca>
2014-06-26 22:35:54 -0400

    heroku push

--- routes/activities.js
+++ routes/activities.js
@@ -4,9 +4,24 @@ var router = express.Router();
 /* GET authenticate redirect */
 router.get('/', function(req, res) 
 {
-    res.render('activities', 
-        { 
-            title: 'Tokens'
+    var activities_url = util.format(
+        'https://api.moves-app.com/api/1.1//user/activities/daily/%s?access_token=%s',
+        '04042014',
+        req.session.moves_access_token);
+
+    request.post(
+        activities_url,
+        { form: { key: 'value' } },
+        function (error, response, body) 
+        {
+            if (!error && response.statusCode == 200) 
+            {
+                res.render('activities', 
+                    { 
+                        title: 'Activities'
+                    }
+                );
+            }
         }
     );
 });

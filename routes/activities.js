56625613722ff7d194c845e7ff1842dbb85d69a5
Jon Hill <jon@jonhill.ca>
2014-06-26 22:32:51 -0400

    heroku push

--- routes/activities.js
+++ routes/activities.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('activities');
+/* GET authenticate redirect */
+router.get('/', function(req, res) 
+{
+    res.render('activities', 
+        { 
+            title: 'Tokens'
+        }
+    );
 });
 
 module.exports = router;

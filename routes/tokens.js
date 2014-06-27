fad602e07d2c58b5be12cb1cee3aee9c6ea3394b
Jon Hill <jon@jonhill.ca>
2014-06-26 22:16:12 -0400

    heroku push

--- routes/tokens.js
+++ routes/tokens.js
@@ -7,7 +7,6 @@ router.get('/', function(req, res)
     res.render('tokens', 
         { 
             title: 'Tokens', 
-            access_token_body : body, 
             moves_access_token : req.session.moves_access_token,
             moves_refresh_token : req.session.moves_refresh_token,
             moves_user_id : req.session.moves_user_id

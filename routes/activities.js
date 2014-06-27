d981f4eeddf8d4c4afedb9fb6b6c691c9565ce16
Jon Hill <jon@jonhill.ca>
2014-06-26 22:06:10 -0400

    heroku push

--- routes/activities.js
+++ routes/activities.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var util = require('util');
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res) {
